bcbde1b85253695f89dd7902056fbf95204cf2c5
in-kim <ehddls960617@gmail.com>
2021-03-17 11:48:59 +0900

    something 페이지 레이아웃 개발

---
+++ src/screens/SomethingScreen/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet} from 'react-native';
+
+export default function SomethingScreen(){
+  return(
+    <View style={styles.container}>
+      <Text>something</Text>
+      <Button
+          title='back'
+          onPress={() => this.props.navigation.goBack()} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+})
